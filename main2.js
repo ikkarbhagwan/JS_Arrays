639b230b59a0f2e4e0f63d3fead9cc04e800d315
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2023-04-20 12:59:53 +0530

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,16 @@
+import {n,show as s,myclass} from "./main.js";
+// import {show} from "./main.js";
+// import{myclass} from"./main.js";
+
+// console.log("hello ..." +n);
+
+document.body.innerHTML=("hello ..." +n);
+
+
+// if you want to call function by any shortcut key then u have to write this Ex.[show as S] =
+// s("ikkar")
+
+
+s("Ikkar");
+
+let ao=new myclass();
